6154414d03c700021ecc51275593ff285ee6c946
revanth <vermareddyrevanth@gmail.com>
2020-11-15 17:53:08 +0530

    refractored the folder structure

--- data/create_review_matrix.py
+++ data/create_review_matrix.py
@@ -4,7 +4,7 @@ import os
 import webbrowser
 
 # Read the dataset into a data table using Pandas
-df = pd.read_csv("movie_ratings_data_set.csv")
+df = pd.read_csv("./csv/movie_ratings_data_set.csv")
 
 # Convert the running list of user ratings into a matrix using the 'pivot table' function
 ratings_df = pd.pivot_table(df, index='user_id', columns='movie_id', aggfunc=np.max)
@@ -13,9 +13,9 @@ ratings_df = pd.pivot_table(df, index='user_id', columns='movie_id', aggfunc=np.
 html = ratings_df.to_html(na_rep="")
 
 # Save the html to a temporary file
-with open("review_matrix.html", "w") as f:
+with open("./html/review_matrix.html", "w") as f:
     f.write(html)
 
 # Open the web page in our web browser
-full_filename = os.path.abspath("review_matrix.html")
+full_filename = os.path.abspath("./html/review_matrix.html")
 webbrowser.open("file://{}".format(full_filename))
--- data/create_review_matrix_as_csv.py
+++ data/create_review_matrix_as_csv.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 # Read the dataset into a data table using Pandas
-df = pd.read_csv("movie_ratings_data_set.csv")
+df = pd.read_csv("./csv/movie_ratings_data_set.csv")
 
 # Convert the running list of user ratings into a matrix using the 'pivot table' function
 ratings_df = pd.pivot_table(df, index='user_id', columns='movie_id', aggfunc=np.max)
 
 # Create a csv file of the data for easy viewing
-ratings_df.to_csv("review_matrix.csv", na_rep="")
+ratings_df.to_csv("./csv/review_matrix.csv", na_rep="")
